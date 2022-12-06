419b1583b19ee3cbaf456d1f117c884c553936a8
Raido Kulla <raido.kulla@gmail.com>
2022-12-06 21:49:27 +0100

    Learning redux

--- src/playground/destructuring.js
+++ src/playground/destructuring.js
@@ -1,1 +1,29 @@
-console.log('destructuring')
+// Arr destructuring
+
+// const address = ['Urb Lomas de Marbella', 'Marbella', 'Malaga', '29603']
+
+// const [street, city, state, zip] = address
+
+// console.log(`You are in ${city}`)
+
+
+// Obj destructuring
+
+// const person = {
+//   name: 'Raido',
+//   age: 34,
+//   location: {
+//     city: 'Marbella',
+//     temp: '16'
+//   }
+// }
+
+// const { name = 'Mikk', age } = person
+
+// console.log(`${name} is ${age}.`)
+
+// const { city, temp: temperature } = person.location
+
+// if (person.location.city && person.location.temp)
+
+//   console.log(`It's ${temperature} in ${city}`)
